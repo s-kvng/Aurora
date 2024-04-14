30ca21c6fdc7d75ae5468670b3acde99854adc8f
s-kvng <kvngnathan8420@gmail.com>
2024-04-14 03:37:01 +0000

    screen:update sign in screen

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
+
 import React from 'react'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
+    <>
+      <Stack>
+        {/* sign in screen */}
+        <Stack.Screen options={{
+          headerShown: false
+        }} name="sign-in"   />
+
+        {/* sign up screen */}
+         <Stack.Screen options={{
+          headerShown: false
+        }} name="sign-up"   />
+      </Stack>
+
+      <StatusBar 
+        backgroundColor='#161622'
+        style='light' 
+      />
+    </>
   )
 }
 
 export default AuthLayout
-
-const styles = StyleSheet.create({})
--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View, ScrollView } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Image } from 'react-native'
+
+import { images } from '../../constants'
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className=' bg-primary h-full '>
+      <ScrollView contentContainerStyle={{ height: "100%"}}>
+        <View className=' w-full h-full justify-center px-4 py-6'>
+        <Image
+          source={images.logo}
+          className='w-[134px] h-[84px]'
+          resizeMode='contain'
+          />
+          <Text className=' text-3xl font-psemibold text-gray-100'>Sign in</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default SignIn
-
-const styles = StyleSheet.create({})
