d77b007f4ca67044af24dd3708ba4ac96014267c
s-kvng <kvngnathan8420@gmail.com>
2024-04-14 02:42:25 +0000

    page: on-boarding page layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 import { Link } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { images } from '../constants';
 
 export default function App() {
   return (
-    <View className='flex-1 justify-center items-center bg-blue-600'>
-      <Text className='text-3xl font-pblack'>Aurora!!!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: "blue"}}>
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className='h-full bg-primary'>
+      <ScrollView contentContainerStyle={{ height: "100%"}} >
+        <View className='w-full h-full  items-center px-4'>
+          <Image
+          source={images.logo}
+          className='w-[134px] h-[84px]'
+          resizeMode='contain'
+          />
+
+        <Image
+          source={images.cards}
+          className='w-full max-w-[380px] h-[300px]'
+          resizeMode='contain'
+        />
+
+        <View className=' relative mt-5'>
+          <Text className=' text-3xl font-bold text-white text-center'>
+              Discover Endless Possibilities with{" "}
+              <Text className='text-secondary-200'>Aora</Text> 
+          </Text>
+          <Image
+          source={images.path}
+          className='absolute w-[130px] h-[15px] -bottom-2 -right-8'
+          resizeMode='contain'
+          />
+        </View>
+
+        <Text className='text-sm font-pregular text-center text-gray-100 mt-7 '>
+          Where Creativity Meets Innovation: 
+          embark on a journey of limitless exploration with Aora</Text>
+        </View>
+      </ScrollView>
+     
+    </SafeAreaView>
   );
 }
 
