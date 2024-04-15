d0646621e85113de8ca0e8b05ceeb4da1ae0d7da
s-kvng <kvngnathan8420@gmail.com>
2024-04-15 04:51:20 +0000

    ui: update sign in screen

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,4 +1,4 @@
-import { Text, View, ScrollView, TouchableOpacity, Pressable } from 'react-native'
+import { Text, View, ScrollView, TouchableOpacity, Platform, KeyboardAvoidingView, StatusBar as status } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Image } from 'react-native'
@@ -7,6 +7,7 @@ import { images } from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
 import { Link } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
 const SignIn = () => {
 
@@ -20,63 +21,66 @@ const SignIn = () => {
   const onSubmit = () => {}
 
   return (
-    <SafeAreaView className=' bg-primary h-full '>
+    <SafeAreaView className={` bg-primary h-full pt-[${status.currentHeight}] `}>
       <ScrollView contentContainerStyle={{ height: "100%"}}>
-        <View className=' w-full h-[83vh] justify-center px-4 py-6'>
-        <Image
-          source={images.logo}
-          className='w-[134px] h-[84px]'
-          resizeMode='contain'
-          />
-          <Text className=' text-3xl font-psemibold text-gray-100'>Sign in to Aora </Text>
+        <KeyboardAvoidingView behavior='padding' keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 0}>
+          <View className=' w-full h-[83vh] justify-center px-4 py-6'>
+          <Image
+            source={images.logo}
+            className='w-[134px] h-[84px]'
+            resizeMode='contain'
+            />
+            <Text className=' text-3xl font-psemibold text-gray-100'>Sign in to Aora </Text>
+
+            <FormField
+            title="Email"
+            placeholder='Enter your email address'
+            value={form.email}
+            handleChangeText={(e) => {
+              setForm({
+              ...form,
+                email: e
+              })
+            }}
+            otherStyles=' mt-10'
+            keyBoardType='email-address'
+            />
 
           <FormField
-          title="Email"
-          placeholder='Enter your email address'
-          value={form.email}
-          handleChangeText={(e) => {
-            setForm({
-            ...form,
-              email: e
-            })
-          }}
-          otherStyles=' mt-10'
-          keyBoardType='email-address'
-          />
+            title="Password"
+            placeholder='Enter your password'
+            value={form.password}
+            handleChangeText={(e) => {
+              setForm({
+              ...form,
+                password: e
+              })
+            }}
+            otherStyles=' mt-7'
+            />
 
-<FormField
-          title="Password"
-          placeholder='Enter your password'
-          value={form.password}
-          handleChangeText={(e) => {
-            setForm({
-            ...form,
-              password: e
-            })
-          }}
-          otherStyles=' mt-7'
-          />
-
-          <View className=' mt-6'>
-            <TouchableOpacity>
-              <Text className='text-gray-500 text-base font-psemibold text-right'>Forgot Password</Text>
-            </TouchableOpacity>
-          </View>
-
-          <CustomButton title='Log in' 
-          handlePress={onSubmit} 
-          isLoading={isSubmitting} 
-          containerStyles='mt-6'  
-          />
-
-          <View className='flex flex-row justify-center items-center pt-5 gap-2'>
-                      <Text className='text-lg font-pregular text-center text-gray-100'>
-                      Dont have an account?
-                      </Text>
-                      <Link href={"/sign-up"} className=' text-lg text-secondary-200 font-psemibold'>Sign Up</Link>
+            <View className=' mt-6'>
+              <TouchableOpacity>
+                <Text className='text-gray-500 text-base font-psemibold text-right'>Forgot Password</Text>
+              </TouchableOpacity>
             </View>
-        </View>
-      </ScrollView>
+
+            <CustomButton title='Log in' 
+            handlePress={onSubmit} 
+            isLoading={isSubmitting} 
+            containerStyles='mt-6'  
+            />
+
+            <View className='flex flex-row justify-center items-center pt-5 gap-2'>
+                        <Text className='text-lg font-pregular text-center text-gray-100'>
+                        Dont have an account?
+                        </Text>
+                        <Link href={"/sign-up"} className=' text-lg text-secondary-200 font-psemibold'>Sign Up</Link>
+              </View>
+          </View>
+          </KeyboardAvoidingView>
+        </ScrollView>
+      
     </SafeAreaView>
   )
 }
