a964b9f2711e6483faea0c5f968fdbc0fc5cf7f2
s-kvng <kvngnathan8420@gmail.com>
2024-04-19 16:57:29 +0000

    logic: write a useAppwrite hook

---
+++ lib/useAppwrite.ts
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import { Alert } from "react-native";
+
+interface AppwriteVideo {
+  // System-generated fields
+  "$collectionId": string;
+  "$createdAt": string;
+  "$databaseId": string;
+  "$id": string;
+  "$permissions": string[]; // Assuming it's an array of strings
+  "$updatedAt": string;
+
+  // Video-specific fields
+  creator: Creator;
+  prompt: string;
+  thumbnail: string;
+  title: string;
+  video: string;
+}
+
+interface Creator {
+  "$collectionId": string;
+  "$createdAt": string;
+  "$databaseId": string;
+  "$id": string;
+  "$permissions": string[]; // Assuming it's an array of strings
+  "$updatedAt": string;
+
+  accountId?: string; // Optional field
+  avatar: string;
+  email: string;
+  username: string;
+}
+
+const useAppwrite = (fn : () => Promise<AppwriteVideo[]>) => {
+  const [data, setData] = useState<AppwriteVideo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  //   fetch post function
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fn();
+      setData(response);
+    } catch (error) {
+      Alert.alert("Error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refresh = () => fetchData();
+
+  return { data, refresh, isLoading };
+};
+
+export default useAppwrite;
