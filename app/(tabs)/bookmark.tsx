b5cee8ded8ff5e0305ebecc10f2a46568d2f7011
s-kvng <kvngnathan8420@gmail.com>
2024-05-07 18:35:09 +0000

    ui: update bookmark screen

--- app/(tabs)/bookmark.tsx
+++ app/(tabs)/bookmark.tsx
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Bookmark = () => {
   return (
-    <View>
-      <Text>bookmark</Text>
-    </View>
+    <SafeAreaView className=' bg-primary h-full justify-center items-center space-y-3'>
+      <Text className=' text-white font-pbold text-2xl'>Bookmark Screen 🔖</Text>
+      <Text className=' text-white font-pbold text-lg'>Under Construction..... 👍 ⛑️</Text>
+    </SafeAreaView>
   )
 }
 
